d9fe8bad6681d25ec11d06304e949a032c7caf41
Gaxve <Gaxve@LAPTOP-SA5T3JQQ>
2019-09-26 18:47:15 +0200

    “Versió 0.2 del projecte prjava01”

--- src/main/java/com/mycompany/prjava01/prjava01.java
+++ src/main/java/com/mycompany/prjava01/prjava01.java
@@ -18,8 +18,9 @@ public class prjava01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        File f = new File("fitxer00.html");
+        File f = new File("fitxer01.html");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
             bw.write("<html>");bw.newLine();
             bw.write("  <head>");bw.newLine();
             bw.write("    <title>");bw.newLine();
